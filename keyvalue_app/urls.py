1e51961e5e22a64b2c25ac60eb2842f36badbad0
unknown <ziad32ahmed111@gmail.com>
2020-04-29 22:36:28 +0200

    add berier token auth

--- keyvalue_app/urls.py
+++ keyvalue_app/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
 from rest_framework import routers
 from . import views
+from rest_framework_simplejwt.views import TokenObtainPairView 
 
 router = routers.DefaultRouter()
 router.register(r'keyvalue-pair', views.KeyValuePairView)
 router.register(r'keyfile-pair', views.KeyFilePairView)
 
 urlpatterns = [
-    path('login/', views.LogInView.as_view(), name='login'),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
     path('sign-up/', views.UserSignUpView.as_view(), name='sign-up'),
     path('', include(router.urls)),
 ]
