a04ab5c8a705ec0806c3b97e9e43989543283975
unknown <ziad32ahmed111@gmail.com>
2020-05-01 03:14:48 +0200

    bariear auth

--- keyvalue_app/urls.py
+++ keyvalue_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 from . import views
-from rest_framework_simplejwt.views import TokenObtainPairView 
+from rest_framework_simplejwt.views import TokenObtainPairView , TokenRefreshView
 
 router = routers.DefaultRouter()
 router.register(r'keyvalue-pair', views.KeyValuePairView)
@@ -9,6 +9,7 @@ router.register(r'keyfile-pair', views.KeyFilePairView)
 
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('sign-up/', views.UserSignUpView.as_view(), name='sign-up'),
     path('', include(router.urls)),
 ]
